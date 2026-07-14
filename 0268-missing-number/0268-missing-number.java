90619bbf6854e2bb2248c1c069f14ab624be23a5
VAIBHAVSINGH-7 <vaibhavgautam422@gmail.com>
2026-07-14 23:49:45 +0530

    Time: 134 ms (8.28%), Space: 47.3 MB (48.71%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,17 +1,28 @@
 class Solution {
+
     public int missingNumber(int[] nums) {
-        for(int i=0;i<=nums.length;i++){
+
+        for(int i = 0; i <= nums.length; i++) {
+
+            // Har number ke liye reset karo
             boolean found = false;
-            for(int j=0;j<nums.length;j++){
-                if(nums[j]==i){
+
+            for(int j = 0; j < nums.length; j++) {
+
+                if(nums[j] == i) {
+
                     found = true;
                     break;
                 }
             }
-            if(found == false){
+
+            // Number nahi mila
+            if(found == false) {
+
                 return i;
             }
         }
+
         return -1;
     }
 }
